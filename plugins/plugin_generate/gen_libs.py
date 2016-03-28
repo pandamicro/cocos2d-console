9fc74f4d4adf536487d56c35de33690207e405b9
zhangbin <zhangbin@cocos2d-x.org>
2016-03-28 17:00:06 +0800

    Add --env parameter for specify environment variables.

--- plugins/plugin_generate/gen_libs.py
+++ plugins/plugin_generate/gen_libs.py
@@ -376,7 +376,7 @@ class LibsCompiler(cocos.CCPlugin):
 
         if not self.disable_strip:
             # strip the android libs
-            ndk_root = os.environ["NDK_ROOT"]
+            ndk_root = cocos.check_environment_variable("NDK_ROOT")
             if cocos.os_is_win32():
                 if cocos.os_is_32bit_windows():
                     check_bits = [ "", "-x86_64" ]
