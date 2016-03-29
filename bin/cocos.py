3d1542d8ee3dbeecf3784765a81f7d58c6c28e11
zhangbin <zhangbin@cocos2d-x.org>
2016-03-29 11:18:11 +0800

    Update the version of cocos command.

--- bin/cocos.py
+++ bin/cocos.py
@@ -31,7 +31,7 @@ from utils import ExtendEnv
 # FIXME: MultiLanguage should be deprecated in favor of gettext
 from MultiLanguage import MultiLanguage
 
-COCOS2D_CONSOLE_VERSION = '2.1'
+COCOS2D_CONSOLE_VERSION = '2.2'
 
 
 class Cocos2dIniParser:
